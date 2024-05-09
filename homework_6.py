3befe09c037ed4e17d5a0b85410830b3259c2f38
AksanaBatyrova <treulott@gmail.com>
2024-05-09 22:49:37 +0300

    fix: linters comments

--- homework_6.py
+++ homework_6.py
@@ -1,11 +1,12 @@
 """
 1. Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
-2. "I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"]
+2. "I love arrays they are my favorite" =>
+   ["I", "love", "arrays", "they", "are", "my", "favorite"]
 3. Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
    Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
-4. Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] 
+4. Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
    сделайте из него строку => "I love arrays they are my favorite"
-5. Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение, 
+5. Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение,
    удалите элемент из списка под индексом 6
 """
 
@@ -23,7 +24,8 @@ print(list(TASK2_STRING.split(" ")))
 Name_Surname = ['Ivan', 'Ivanou']
 CITY = 'Minsk'
 COUNTRY = 'Belarus'
-print(f"Привет, {' '.join(Name_Surname)}! Добро пожаловать в {City} {Country}!")
+print(f"Привет, {' '.join(Name_Surname)}! "
+      f"Добро пожаловать в {CITY} {COUNTRY}!")
 
 
 # Task 4
@@ -31,7 +33,7 @@ task4_string = ["I", "love", "arrays", "they", "are", "my", "favorite"]
 print(' '.join(task4_string))
 
 # Task 5
-arr = ['Apple', 'Orange', 'Pear', 'Cherry', 'Banana', 'Mango', 'Grapes', 'Lime', 
+arr = ['Apple', 'Orange', 'Pear', 'Cherry', 'Banana', 'Mango', 'Grapes', 'Lime',
       'Strawberry', 'Blackcurrant']
 arr[2] = 'Plum'
 del arr[6]
