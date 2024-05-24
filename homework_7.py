37f30d33b3620f7f8a19ae1935c9a2ba48082989
AksanaBatyrova <treulott@gmail.com>
2024-05-24 13:44:51 +0300

    updating hw according to comments

--- homework_7.py
+++ homework_7.py
@@ -1,44 +1,73 @@
-"""
-Быки и коровы
-B классическом варианте игра рассчитана на двух игроков.
-Каждый из игроков задумывает и записывает тайное 4-значное число c
-неповторяющимися цифрами. Игрок, который начинает игру по жребию,
-делает первую попытку отгадать число.
-Попытка — это 4-значное число c неповторяющимися цифрами, сообщаемое
-противнику. Противник сообщает в ответ, сколько цифр угадано без
-совпадения c их позициями в тайном числе (то есть количество коров)
-и сколько угадано вплоть до позиции в тайном числе (то есть
-количество быков). При игре против компьютера игрок вводит
-комбинации одну за другой, пока не отгадает всю последовательность.
-Ваша задача реализовать программу, против которой можно сыграть
-в "Быки и коровы"
-"""
+"""This module is a homework 7 made by Aksana Batyrova"""
 
-ANSWER = 2310
-first_guess = input('Make a guess: ')
+import random
+
+# Быки и коровы
+# B классическом варианте игра рассчитана на двух игроков.
+# Каждый из игроков задумывает и записывает тайное 4-значное число c
+# неповторяющимися цифрами. Игрок, который начинает игру по жребию,
+# делает первую попытку отгадать число.
+# Попытка — это 4-значное число c неповторяющимися цифрами, сообщаемое
+# противнику. Противник сообщает в ответ, сколько цифр угадано без
+# совпадения c их позициями в тайном числе (то есть количество коров)
+# и сколько угадано вплоть до позиции в тайном числе (то есть
+# количество быков). При игре против компьютера игрок вводит
+# комбинации одну за другой, пока не отгадает всю последовательность.
+# Ваша задача реализовать программу, против которой можно сыграть
+# в "Быки и коровы"
+
+
+def generate_random_number():
+    """function generating a number of 4 random unique digits"""
+    number = random.randint(1000, 9999)
+    if len(set(map(int, str(number)))) != 4:
+        result = generate_random_number()
+    else:
+        result = list(map(int, str(number)))
+    return result
+
+
+def make_a_guess():
+    """function asking user to enter their guess and checking it"""
+    try:
+        first_guess = int(input('Make a guess: '))
+    except ValueError:
+        print('Invalid input. Please enter only digits')
+        result = make_a_guess()
+    else:
+        if len(list(map(int, str(first_guess)))) != 4:
+            print('Invalid input. Please enter 4 unique digits')
+            result = make_a_guess()
+        if len(set(map(int, str(first_guess)))) != 4:
+            print('Invalid input. Please enter 4 unique digits')
+            result = make_a_guess()
+        else:
+            result = list(map(int, str(first_guess)))
+    return result
 
 
 def bulls_and_cows_game(answer, guess):
     """function describing 'cows and bulls' game"""
     cows = 0
     bulls = 0
-    answer_li = list(str(answer))
-    guess_li = list(guess)
     for i in range(4):
-        if guess_li[i] == answer_li[i]:
+        if guess[i] == answer[i]:
             bulls += 1
-        elif guess_li[i] in answer_li:
+        elif guess[i] in answer:
             cows += 1
     print(f'{cows} cows, {bulls} bulls')
     if bulls == 4:
-        return 'You win!'
+        result = print('You win!')
     if bulls != 4:
         guess = input('Make another guess: ')
-        return bulls_and_cows_game(answer, guess)
-    return None
+        result = bulls_and_cows_game(answer, list(map(int, str(guess))))
+    return result
 
 
-print(bulls_and_cows_game(ANSWER, first_guess))
+generated_answer = generate_random_number()
+user_guess = make_a_guess()
+print(bulls_and_cows_game(generated_answer, user_guess))
+
 
 # Пирамида
 # Мы можем визуализировать художественную пирамиду ASCII c N уровнями,
